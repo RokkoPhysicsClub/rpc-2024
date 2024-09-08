9ed433beb8557851e82ed6141e51b443bcf713ac
0b1001100 <okmstsvnx.os@gmail.com>
2024-09-08 23:24:17 +0900

    initial commit.

---
+++ src/consts.ts
@@ -0,0 +1,47 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const BASE_URL= 'https://rokkophysicsclub.github.io/rpc-2024/';
+export const SITE_TITLE = '六甲学院物理部2024';
+export const SITE_DESCRIPTION = '六甲学院物理部の2024年度オンライン文化祭サイトです。（学校非公式）';
+export const SITE_MENU = [
+  {
+    "name":"Welcome",
+    "link":"/"
+  },
+  {
+    "name":"Works",
+    "link":"/works/index.html"
+  },
+  {
+    "name":"Links",
+    "link":"/links/index.html"
+  },
+  {
+    "name":"About",
+    "link":"/about/index.html"
+  }
+];
+export const SOCIALS = [
+  {
+    "icon":"/img/x-twitter.svg",
+    "link":"https://x.com/RokkoPhysics"
+  },
+  {
+    "icon":"/img/bluesky.svg",
+    "link":null
+  },
+  {
+    "icon":"/img/github.svg",
+    "link":"https://github.com/RokkoPhysicsClub"
+  },
+  {
+    "icon":"/img/youtube.svg",
+    "link":"https://www.youtube.com/channel/UCvLDGaFKdbpQ7vELdrUSyCw"
+  },
+  {
+    "icon":"/img/niconico.svg",
+    "link":"https://sp.nicovideo.jp/user/88466335"
+  }
+]
+export const SITE_CREDIT = '2024 六甲学院物理部';
